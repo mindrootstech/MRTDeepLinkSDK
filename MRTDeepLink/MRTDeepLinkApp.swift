acffd09b78e7b84074d833e27e307892dace2b1c
dev-blupalms <dev@blupalms.com>
2026-06-15 10:38:12 +0530

    Initial Commit

---
+++ MRTDeepLink/MRTDeepLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MRTDeepLinkApp.swift
+//  MRTDeepLink
+//
+//  Created by Gurpreet Singh on 15/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MRTDeepLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MRTDeepLinkTests/MRTDeepLinkTests.swift
@@ -0,0 +1,17 @@
+//
+//  MRTDeepLinkTests.swift
+//  MRTDeepLinkTests
+//
+//  Created by Gurpreet Singh on 15/06/26.
+//
+
+import Testing
+@testable import MRTDeepLink
+
+struct MRTDeepLinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
